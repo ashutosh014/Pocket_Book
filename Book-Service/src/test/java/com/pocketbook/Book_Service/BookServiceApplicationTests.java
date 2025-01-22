2b8edd9e99ebc2d73e4563aecdf5ea6ea74301a5
Vivekanand Reshmi <vivekanand.10749650@ltimindtree.com>
2025-01-22 15:39:54 +0530

    Created Book-Service

---
+++ Book-Service/src/test/java/com/pocketbook/Book_Service/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocketbook.Book_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
