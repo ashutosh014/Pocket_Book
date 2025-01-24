e5f141fcc71ec4fea1bb4437b88fdba45e3bddd1
Ashutosh Pattnaik <giet16ee014@gmail.com>
2025-01-24 16:04:10 +0530

    Added unit tets for User-Service

--- Pocket_Book/User-Service/src/main/java/com/pocketbook/userservice/service/UserServiceIMPL.java
+++ Pocket_Book/User-Service/src/main/java/com/pocketbook/userservice/service/UserServiceIMPL.java
@@ -6,6 +6,9 @@ import com.pocketbook.userservice.exceptions.UserNamePresentException;
 import com.pocketbook.userservice.exceptions.UserNotFoundException;
 import com.pocketbook.userservice.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,15 +18,51 @@ import java.util.Optional;
 @Transactional
 public class UserServiceIMPL implements UserService{
 
+    private static final Logger Logger = LoggerFactory.getLogger(UserServiceIMPL.class);
+
     private final UserRepository userRepository;
 
     public UserServiceIMPL(UserRepository userRepository) {
         this.userRepository = userRepository;
+        Logger.info("Inside the User Service IMPL class");
     }
 
     @Override
     public User createUser(User user) throws UserNamePresentException, UserEmailPresentException {
         // Check if the username and user email is not present before in the database
+        if(!isUserNameUnique(user.getUserName())) {
+            Logger.error("Username {} is already present ", user.getUserName());
+            throw new UserNamePresentException("User Name already exists. Kindly enter a different user name.");
+        }
+
+        if(!isUserEmailUnique(user.getUserEmail())) {
+            Logger.error("Email address : {} is already present", user.getUserEmail());
+            throw new UserEmailPresentException("Email address is already in use. Kindly use a different email address");
+        }
+
+        Logger.info("Saving the user in the database");
+        return userRepository.save(user);
+    }
+
+
+    @Override
+    public List<User> getAllUsers() {
+        Logger.info("Fetching all users");
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserById(Long id) throws UserNotFoundException {
+        Logger.info("Fetching user details by user id : {}", id);
+        return userRepository.findById(id).orElseThrow(() ->
+                new UserNotFoundException("No user found with id : " + id));
+    }
+
+    @Override
+    public User updateUser(Long id,User user) throws UserNotFoundException, UserNamePresentException, UserEmailPresentException {
+        Logger.info("Fetching user details by user id : {}", id);
+        Optional<User> userDetails = userRepository.findById(id);
+
         if(!isUserNameUnique(user.getUserName())) {
             throw new UserNamePresentException("User Name already exists. Kindly enter a different user name.");
         }
@@ -32,25 +71,7 @@ public class UserServiceIMPL implements UserService{
             throw new UserEmailPresentException("Email address is already in use. Kindly use a different email address");
         }
 
-        return userRepository.save(user);
-    }
-
-
-    @Override
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
-
-    @Override
-    public User getUserById(Long id) throws UserNotFoundException {
-        return userRepository.findById(id).orElseThrow(() ->
-                new UserNotFoundException("No user found with id : " + id));
-    }
-
-    @Override
-    public User updateUser(Long id,User user) throws UserNotFoundException, UserNamePresentException, UserEmailPresentException {
-        Optional<User> userDetails = userRepository.findById(id);
-
+        Logger.info("Updating the user details");
         if(userDetails.isPresent()) {
             User updatedUser = User.builder()
                     .userId(id)
@@ -58,13 +79,6 @@ public class UserServiceIMPL implements UserService{
                     .userEmail(userDetails.get().getUserEmail())
                     .build();
 
-            if(!isUserNameUnique(updatedUser.getUserName())) {
-                throw new UserNamePresentException("User Name already exists. Kindly enter a different user name.");
-            }
-
-            if(!isUserEmailUnique(updatedUser.getUserEmail())) {
-                throw new UserEmailPresentException("Email address is already in use. Kindly use a different email address");
-            }
 
             return userRepository.save(updatedUser);
         }
@@ -75,8 +89,14 @@ public class UserServiceIMPL implements UserService{
     public void deleteUser(Long id) throws UserNotFoundException {
         Optional<User> userDetails = userRepository.findById(id);
 
-        userDetails.ifPresent(userRepository::delete);
-        throw new UserNotFoundException("No user found with id : " + id);
+        Logger.info("Deleting the user");
+        if(userDetails.isPresent()) {
+            userRepository.delete(userDetails.get());
+        }
+        else {
+            throw new UserNotFoundException("No user found with id : " + id);
+        }
+
     }
 
 
